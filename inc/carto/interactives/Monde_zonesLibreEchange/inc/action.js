3b2d72a05c52ae7eeedb24554a41e7057819cc7d
JulieMorineau <164333044+JulieMorineau@users.noreply.github.com>
2024-11-18 20:29:17 +0100

    Add files via upload

---
+++ inc/carto/interactives/Monde_zonesLibreEchange/inc/action.js
@@ -0,0 +1,200 @@
+	let togg1 = document.getElementById("togg1");
+	let togg2 = document.getElementById("togg2");
+	let d1 = document.getElementById("d1");
+	let d2 = document.getElementById("d2");
+	let togg3 = document.getElementById("togg3");
+	let togg4 = document.getElementById("togg4");
+	let d3 = document.getElementById("d3");
+	let d4 = document.getElementById("d4");
+	let togg5 = document.getElementById("togg5");
+	let togg6 = document.getElementById("togg6");
+	let d5 = document.getElementById("d5");
+	let d6 = document.getElementById("d6");
+	let togg7 = document.getElementById("togg7");
+	let togg8 = document.getElementById("togg8");
+	let d7 = document.getElementById("d7");
+	let d8 = document.getElementById("d8");
+	let togg9 = document.getElementById("togg9");
+	let togg10 = document.getElementById("togg10");
+	let d9 = document.getElementById("d9");
+	let d10 = document.getElementById("d10");
+	let togg11 = document.getElementById("togg11");
+	let togg12 = document.getElementById("togg12");
+	let d11 = document.getElementById("d11");
+	let d12 = document.getElementById("d12");
+	let togg13 = document.getElementById("togg13");
+	let togg14 = document.getElementById("togg14");
+	let d13 = document.getElementById("d13");
+	let d14 = document.getElementById("d14");
+	let togg15 = document.getElementById("togg15");
+	let togg16 = document.getElementById("togg16");
+	let d15 = document.getElementById("d15");
+	let d16 = document.getElementById("d16");
+	let togg17 = document.getElementById("togg17");
+	let togg18 = document.getElementById("togg18");
+	let d17 = document.getElementById("d17");
+	let d18 = document.getElementById("d18");
+	
+	togg1.addEventListener("click", () => {
+		if (getComputedStyle(d1).display != "none") {
+			d1.style.display = "none";
+		} else {
+			d1.style.display = "block";
+		}
+	togg1.classList.toggle("active-button"); 
+	});
+	
+	togg2.addEventListener("click", () => {
+		if (getComputedStyle(d2).display != "none") {
+			d2.style.display = "none";
+		} else {
+			d2.style.display = "block";
+		}
+		togg2.classList.toggle("active-button"); 
+	});
+	
+	togg3.addEventListener("click", () => {
+		if (getComputedStyle(d3).display != "none") {
+			d3.style.display = "none";
+		} else {
+			d3.style.display = "block";
+		}
+		togg3.classList.toggle("active-button"); 
+	});
+	
+	togg4.addEventListener("click", () => {
+		if (getComputedStyle(d4).display != "none") {
+			d4.style.display = "none";
+		} else {
+			d4.style.display = "block";
+		}
+		togg4.classList.toggle("active-button"); 
+	});
+	
+	togg5.addEventListener("click", () => {
+		if (getComputedStyle(d5).display != "none") {
+			d5.style.display = "none";
+		} else {
+			d5.style.display = "block";
+		}
+		togg5.classList.toggle("active-button"); 
+	});
+	
+	togg6.addEventListener("click", () => {
+		if (getComputedStyle(d6).display != "none") {
+			d6.style.display = "none";
+		} else {
+			d6.style.display = "block";
+		}
+		togg6.classList.toggle("active-button"); 
+	});
+	
+	togg7.addEventListener("click", () => {
+		if (getComputedStyle(d7).display != "none") {
+			d7.style.display = "none";
+		} else {
+			d7.style.display = "block";
+		}
+		togg7.classList.toggle("active-button"); 
+	});
+	
+	togg8.addEventListener("click", () => {
+		if (getComputedStyle(d8).display != "none") {
+			d8.style.display = "none";
+		} else {
+			d8.style.display = "block";
+		}
+		togg8.classList.toggle("active-button"); 
+	});
+	
+	togg9.addEventListener("click", () => {
+		if (getComputedStyle(d9).display != "none") {
+			d9.style.display = "none";
+		} else {
+			d9.style.display = "block";
+		}
+		togg9.classList.toggle("active-button"); 
+	});
+	
+	togg10.addEventListener("click", () => {
+		if (getComputedStyle(d10).display != "none") {
+			d10.style.display = "none";
+		} else {
+			d10.style.display = "block";
+		}
+		togg10.classList.toggle("active-button");
+	});
+	
+	togg11.addEventListener("click", () => {
+		if (getComputedStyle(d11).display != "none") {
+			d11.style.display = "none";
+		} else {
+			d11.style.display = "block";
+		}
+		togg11.classList.toggle("active-button"); 
+	});
+	
+	togg12.addEventListener("click", () => {
+		if (getComputedStyle(d12).display != "none") {
+			d12.style.display = "none";
+		} else {
+			d12.style.display = "block";
+		}
+		togg12.classList.toggle("active-button"); 
+	});
+
+	togg13.addEventListener("click", () => {
+		if (getComputedStyle(d13).display != "none") {
+			d13.style.display = "none";
+		} else {
+			d13.style.display = "block";
+		}
+		togg13.classList.toggle("active-button"); 
+	});
+
+	togg14.addEventListener("click", () => {
+		if (getComputedStyle(d14).display != "none") {
+			d14.style.display = "none";
+		} else {
+			d14.style.display = "block";
+		}
+		togg14.classList.toggle("active-button"); 
+	});
+
+	togg15.addEventListener("click", () => {
+		if (getComputedStyle(d15).display != "none") {
+			d15.style.display = "none";
+		} else {
+			d15.style.display = "block";
+		}
+		togg15.classList.toggle("active-button"); 
+	});
+
+	togg16.addEventListener("click", () => {
+		if (getComputedStyle(d16).display != "none") {
+			d16.style.display = "none";
+		} else {
+			d16.style.display = "block";
+		}
+		togg16.classList.toggle("active-button");
+	});
+
+	togg17.addEventListener("click", () => {
+		if (getComputedStyle(d17).display != "none") {
+			d17.style.display = "none";
+		} else {
+			d17.style.display = "block";
+		}
+		togg17.classList.toggle("active-button"); 
+	});
+
+	togg18.addEventListener("click", () => {
+		if (getComputedStyle(d18).display != "none") {
+			d18.style.display = "none";
+		} else {
+			d18.style.display = "block";
+		}
+		togg18.classList.toggle("active-button"); 
+	});
+
+
